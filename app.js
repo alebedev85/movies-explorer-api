0d2bf25d6d3dbce7889963a0cd26e50879454455
Andrey Lebedev <alebedev424@gmail.com>
2023-07-03 15:06:01 +0300

    refactor: добавил файлы

---
+++ app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const express = require('express');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+const router = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1/bitfilmsdb' } = process.env;
+
+mongoose.connect(MONGO_URL)
+  .then(() => console.log('База данных подключена'))
+  .catch((err) => console.log('Ошибка подключения к БД', err));
+
+const app = express();
+app.use(cors());
+
+app.use(helmet());
+app.use(express.json());
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use(router);
+
+app.use(errorLogger);
+
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ middlewares/validate.js
@@ -0,0 +1,55 @@
+const { celebrate, Joi } = require('celebrate');
+const { regexHttp, regexEmail } = require('../utils/constants');
+
+const validateUserBody = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().pattern(regexEmail),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(regexHttp),
+  }),
+});
+
+const validateEditUserInfo = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+});
+
+const validateEditUserAvatar = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().pattern(regexHttp),
+  }),
+});
+
+const validateCardBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(regexHttp),
+  }),
+});
+
+const validationUserId = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().required().alphanum().hex()
+      .length(24),
+  }),
+});
+
+const validationCardId = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().alphanum().hex()
+      .length(24),
+  }),
+});
+
+module.exports = {
+  validateUserBody,
+  validateCardBody,
+  validateEditUserInfo,
+  validateEditUserAvatar,
+  validationUserId,
+  validationCardId,
+};
